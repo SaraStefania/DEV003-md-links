2342fb3026fece9d0816dd0078711f855360ab6b
sara040616 <sara.sierraramos@gmail.com>
2023-04-02 14:58:31 -0500

    Primer intento de crear la funcion mdLinks con funciones individuales

--- src/index.js
+++ src/index.js
@@ -1,34 +1,59 @@
-const { routerValidate,
-     absolutePath,
-    convertAbsolute,
-    validateFile,
-    directorio } = require('./api')
-// const fs = require('fs');
-// //  se incluye modulo de ruta
-// const path = require('path')
-
+const { routerValidate, convertAbsolute, validateFile,  extractLinks, filesMdDirectorios, getStatus, extractLinksFromDirectory } = require('./api');
 const absoluta = '/Users/jessira/Desktop/DEV003-md-links/README.md'
-const directorios  = '/Users/jessira/Desktop/DEV003-md-links'
+const directorios1  = '/Users/jessira/Desktop/DEV003-md-links'
 const relativa = './README.md'
 const falsa = '/rutaFalsa'
+const directorios2 = '/Users/jessira/Desktop/pruebas/red social'
+  
+const mdLinks = (path, options) => {
+    return new Promise((resolve, reject) => {
+        //convierte en absoluta
+      const absoluteConvert = convertAbsolute(path);
+      //valida si la ruta existe
+      if (!routerValidate(absoluteConvert)) {
+        reject('La ruta no existe');
+      }
+  //valida si es un archivo
+      const checkFile = validateFile(absoluteConvert);
+      //trae los archivos .md
+      const isMdFile = filesMdDirectorios(absoluteConvert);
+  //si es un archivo y contiene archivos .md
+      if (checkFile === 'Es un archivo' && isMdFile) {
+        //extrae los links 
+        extractLinks(absoluteConvert)
+          .then((links) => {
+            //si la opcion es true 
+            if (options && options.validate) {
+            //se llama a la funcion getStatus
+                resolve(getStatus(links))
+              } else {
+                //se resuelve la promesa con los links extraidos
+                resolve(links);
+              }
+          })
+          //si es un directorio y contiene archivos .md
+      } else if (checkFile === 'No es un archivo' && isMdFile) {
+        //extrae los links
+        extractLinksFromDirectory(absoluteConvert)
+          .then((links) => {
+            //si la opcion es true 
+            if (options && options.validate) {
+                //se llama la funcion getStatus
+                resolve(getStatus(links))
+              } else {
+                //se resuelve la promesa con los links extraidos
+                resolve(links);
+              }
+          })
+      } else {
+        //se rechaza la promesa
+        reject('No se encontraron archivos .md');
+      }
+    });
+  };
+  
+mdLinks('/Users/jessira/Desktop/DEV003-md-links', {validate: true})
+  .then((linksData) => console.log(linksData))
+  .catch((error) => console.log(error));
 
-// // crear funcion que acepte dos argumentos
- const mdLinks = (path, options) => {
-//  retorna una nueva promesa
- return new Promise((resolve, reject) => {
-if(routerValidate(path)){
-    console.log('Es valida')
-}if(absolutePath(path)){
-console.log('La ruta es absoluta')
-}if(convertAbsolute(path)){
-    console.log('Se convirtio la ruta a absoluta')
-}if(validateFile(path)){
-    console.log('Es un archivo')
-}if(directorio(path)){
-    console.log('Es un directorio ')
-}
- })
- }
- console.log(mdLinks(directorios))
-
-module.exports = mdLinks
+module.exports = mdLinks;
