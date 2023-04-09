e8865b9d4153c3595f6e657fbec23c0f41b9fc05
sara040616 <sara.sierraramos@gmail.com>
2023-04-09 10:12:58 -0500

    Realizando test de funciones independientes y arreglando la cobertura de algunas líneas

--- test/api.spec.js
+++ test/api.spec.js
@@ -1,7 +1,7 @@
 const { routerValidate, absolutePath, convertAbsolute, validateFile, directorio, extractLinks, filesMd, getStatus}  = require('../src/api');
-const absoluta = '/Users/jessira/Desktop/DEV003-md-links/README.md'
-const directorios  = '/Users/jessira/Desktop/DEV003-md-links'
-const relativa = './README.md'
+const absoluta = '/Users/jessira/Desktop/DEV003-md-links/prueba/prueba.md'
+const directorios  = '/Users/jessira/Desktop/DEV003-md-links/prueba'
+const relativa = 'prueba/prueba.md'
 const urls = 
 [
   {
@@ -20,9 +20,6 @@ const urls =
     file: '/Users/jessira/Desktop/DEV003-md-links/README.md'
   }
 ]
-
-//-----------mock---------------
-console.log(getStatus(urls))
 
 //-----------------Validar si la ruta existe------------------
 describe('routerValidate', () => {
@@ -57,6 +54,7 @@ describe('validateFile', () =>{
 })
 
 //--------------------------Validar si es un Directorio-------------------------
+
 describe('directorio', () =>{
   it('Deberia devolver un array con nombre del archivo y directorio', () =>{
     const arrayOfFiles = directorio(directorios)
@@ -64,38 +62,37 @@ describe('directorio', () =>{
   })
 }) 
 
+
 //--------------------------Traer href, text y file-------------------------
 describe('extractLinks', () => {
-  it('Deberia retonar un array con text, href y files', () => {
+  it('Deberia retonar un array con text, href y files de un archivo', () => {
     return extractLinks(absoluta)
       .then((links) => {
         expect(Array.isArray(links)).toBe(true);
+        //primer elemento del arreglo
         expect(links[0]).toEqual({
           text: 'Markdown',
           href: 'https://es.wikipedia.org/wiki/Markdown',
-          file: '/Users/jessira/Desktop/DEV003-md-links/README.md'
+          file: '/Users/jessira/Desktop/DEV003-md-links/prueba/prueba.md'
         });
         
       });
   });
-
- it('Deberia mostrar un error si no se puede leer el contenido', () =>{
-   const result = extractLinks('./noexiste')
+})
+it('Deberia retornar un error si no se pudo leer los archivos', () =>{
+  const result  = extractLinks('./noexiste')
    //se espera que devuelva un error que sea igual al error original
    return expect(result).rejects.toEqual('No se pudo leer uno o más archivos')
-  })
-  
 })
 
 //----------------------- Validar archivos Markdown-------------------------
 describe('filesMd', () =>{
-  it('Deberia validar si es un archivo Markdown', () =>{
+  it('Deberia retornar true si contiene archivos Markdown', () =>{
     expect(filesMd(absoluta)).toEqual(true)
   })
-  it('Deberia validar si no es un archivo Markdown', () =>{
-    expect(filesMd('/Users/jessira/Desktop/DEV003-md-links/package.json')).toEqual(false)
+  it('Deberia retornar false si no contiene archivos Markdown', () =>{
+    expect(filesMd('./sinArchivos.js')).toEqual(false)
   })
-
 })
 
 //-------------------- Status--------------------------
