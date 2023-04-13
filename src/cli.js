7eaf91c80ebb6db3ad2383555ac069eb36f75e13
sara040616 <sara.sierraramos@gmail.com>
2023-04-12 22:17:17 -0500

    Se añadio comando help al cli

--- src/cli.js
+++ src/cli.js
@@ -7,9 +7,26 @@ const colors = require('colors');
 // es un arreglo que contiene los argumentos de linea de comando.
 const path = process.argv[2];
 const options = {
+  help: process.argv.includes('--help'),
   validate: process.argv.includes('--validate'),
   stats: process.argv.includes('--stats')
 };
+
+if(options.help){
+  console.log('\n---------------------------------------')
+  console.log('"mdLinks"'.bgYellow ,'es una libreria que te permite verificar el estado de los enlaces de los archivos Markdown.')
+  console.log('---------------------------------------')
+  console.log(`\n${'·'.grey} A continuación te mostraré los pasos a seguir y los comandos que debes usar.`.bgBlack)
+  console.log(`\n${'1.'.grey}   Ingresa la ruta de tu archivo o directorio que deseas analizar`)
+  console.log(`\n${'2.'.grey}   Después de esto, ingresa una de las siguientes opciones:\n`)
+  console.log('---------------------------------------')
+  console.log('--validate'.green, ':', 'Obtendrás el estado de los enlaces de tus archivos') 
+  console.log('---------------------------------------')
+  console.log('--stats'.green, ':', 'Obtendrás las estadísticas del total de enlaces y los enlaces únicos')
+  console.log('---------------------------------------')
+  console.log('--validate --stats'.green, ':', 'Obtendrás las estadísticas de los enlaces rotos')
+  console.log('---------------------------------------')
+}
 
 
 
@@ -44,11 +61,11 @@ mdLinks(path, options)
     }else{
       result.flat().forEach(links => {
         console.log('---------------------------------------')
-        console.log( 'Text:'.cyan ,links.text);
-        console.log('Href:'.cyan ,links.href);
-        console.log('File:'.cyan ,links.file);
-        console.log('---------------------------------------')
-      });
+      console.log( 'Text:'.cyan ,links.text);
+      console.log('Href:'.cyan ,links.href);
+      console.log('File:'.cyan ,links.file);
+      console.log('---------------------------------------')
+      })
     }
   })
   .catch((error) => {
